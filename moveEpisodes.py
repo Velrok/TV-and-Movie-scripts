41fcd49e23da93822b2fd47ae9bca704c02236d0
Waldemar Schwan <waldemar.schwan@gmail.com>
2011-06-16 12:47:10 +0200

    added some comments. complete the source to move a single file

--- moveEpisodes.py
+++ moveEpisodes.py
@@ -20,34 +20,47 @@ Options:
 """
 	sys.exit()
 	
-	
+# get source file or directory from argument 1
 src = sys.argv[1]
+# get destination directory from argument 2
 dest = sys.argv[2]
 
+# tvnamer default pattern
 pattern = '([\w ]*) - \[(\d+)x(\d+)\] (.*)'
 reg_ex = re.compile(pattern)
 
+
 def move(filename):
-#	print src_file
+	"""
+	Moves the given filename into the destination dir. Creating subdirectorys for
+	series name and season.
+	"""
 	m = re.search(reg_ex, filename)
-	name = m.group(1)
+	name = m.group(1)	# get series name
 	season = "Season"
-	season += m.group(2)
+	season += m.group(2) # get season number
 	
-	destination_directory = os.path.join(dest, name, season)
-	target = os.path.join(destination_directory, filename)
-	src_file = os.path.join(src, filename)
+	destination_directory = os.path.join(dest, name, season)	#create desination path
+	target = os.path.join(destination_directory, filename)		#create target filename
+	src_file = os.path.join(src, filename)										#get source filename
 	
+	# if destination dir doesn't exist create it
 	if(not os.path.exists(destination_directory)):
 		os.makedirs(destination_directory)
 	#os.link(src_file,target)
-	shutil.move(src_file, target)
+	
+	shutil.move(src_file, target)		# move the file
+	
+## end of move function
 
 
 if(os.path.isfile(src)):
+	# source is a single file get and move it
 	filename = os.path.basename(src)
+	move(filename)
 	
 elif(os.path.isdir(src)):
+	# source is a directoy, look for all none hidden files and move them one by one
 	dir_list = os.listdir(src)
 	for f in dir_list:
 		if f[0] == '.':
